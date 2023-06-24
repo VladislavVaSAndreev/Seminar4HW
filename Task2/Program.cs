8d19c2bff9e1a338a651be484febe1cc1e9b4622
VladislavAndreev <andreev.92@inbox.ru>
2023-06-24 15:33:43 +0300

    addTask2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Извините, но вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+int number = Prompt("Введите число");
+
+int sum =0;
+int count = 0;
+while (0<number)
+{
+   count=number%10;
+   number=number/10;
+   sum+=count;
+}
+System.Console.WriteLine(sum);
