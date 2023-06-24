467d0988f3373cb4d4af1fb3fa8efb01392082f9
VladislavAndreev <andreev.92@inbox.ru>
2023-06-24 15:36:25 +0300

    addTask3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] GenerateArray(int len, int min, int max)
+{
+int[] list1 = new int[len];
+Random rnd = new Random();
+for (int i = 0; i < list1.Length; i++)
+{
+list1[i] = rnd.Next(min, max + 1);
+}
+return list1;
+}
+void PrintArrey(int[] Arrey)
+{
+for (int i = 0; i < Arrey.Length; i++)
+{
+System.Console.Write(Arrey[i] + " ");
+}
+}
+int redin(string tex)
+{
+    Console.WriteLine(tex);
+    string number = Console.ReadLine();
+    int L = Convert.ToInt32(number);
+    int mi = Convert.ToInt32(number);
+    int ma = Convert.ToInt32(number);
+    return L;
+    return mi;
+    return ma;
+}
+int L = redin($"Введите длинну массива");
+int mi = redin($"Введите минимальное число массива");
+int ma = redin($"Введите максимальное число массива");
+int[] number = GenerateArray(L, mi, ma);
+PrintArrey(number);
